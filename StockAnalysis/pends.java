189fcb26c5c547f968046919ed99625ce58cac86
Milo Pelletier <milopelle08@email.com>
2023-02-25 15:29:07 -0500

    continued loading saved stocks

--- StockAnalysis/pends.java
+++ StockAnalysis/pends.java
@@ -1,7 +1,5 @@
 package StockAnalysis;
 
 public class pends {
-    public static int ba;
-    public static int bb;
 
 }
--- StockAnalysis/saves.java
+++ StockAnalysis/saves.java
@@ -1,27 +1,41 @@
 package StockAnalysis;
 
+import org.jsoup.Jsoup;  
+import org.jsoup.nodes.Document;
 import java.io.BufferedReader;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class saves {
-    public static int aa;
-    public static int ab;
+
     public void gatherSavedStocks() throws FileNotFoundException, IOException {
         System.out.println("Gathering Saved Stocks...");
         System.out.println("...");
         System.out.println("...");
         System.out.println("...");
-       
-        try{
-            BufferedReader gatherStocks = new BufferedReader(new FileReader("stockNames.txt"));
-            
 
-            
-        }finally{
-            
+    
+    }
+    
+    public static void gatherLoop() throws FileNotFoundException, IOException{
+
+        BufferedReader gatherStocks = new BufferedReader(new FileReader("stockNames.txt"));
+        String observer = gatherStocks.readLine();
+        
+        if(observer.equals("")){
+            gatherStocks.close();
+        }else{
+            Document doc = Jsoup.connect(observer).get();
+            Elements contentA = doc.getElementsByClass("D(ib) Fz(18px)");
+            System.out.println("|------------------------------------------------|");
+            System.out.println();
+            for(Element a: contentA){
+            System.out.println("| Name: "+a.text());
         }
+    }
     }
 }
 
