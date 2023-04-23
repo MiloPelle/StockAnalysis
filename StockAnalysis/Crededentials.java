d1803425abd843c22137ffdb5699a4e3bc035836
Milo Pelletier <milopelle08@email.com>
2023-04-23 18:50:18 -0400

    THIS IS THE REAL LAST REVISION

--- StockAnalysis/Crededentials.java
+++ StockAnalysis/Crededentials.java
@@ -19,6 +19,7 @@ public class Crededentials{
         System.out.printf("| 4. Peck3277Peck3277 1, et al. “Handling Connection Errors and Jsoup.” Stack Overflow, 1 Feb. 1959, https://stackoverflow.com/questions/10245519/handling-connection-errors-and-jsoup. |%n");
         System.out.printf("| 5. Bufferedwriter (Java Platform SE 8 ) - oracle. (n.d.). Retrieved April 18, 2023, from https://docs.oracle.com/javase/8/docs/api/java/io/BufferedWriter.html |%n");
         System.out.printf("| 6. YouTube. (2018, May 20). How to web scrape stock data with Java using the jsoup library. YouTube. Retrieved April 23, 2023, from https://www.youtube.com/watch?v=tI1qGwhn_bs |%n");
+        System.out.printf("| 7. KitKit 111 bronze badge, &amp; 18.7k66 gold badges3232 silver badges4141 bronze badges. (1969, December 1). Jsoup and for Loop Nosuchelementexception. Stack Overflow. Retrieved April 23, 2023, from https://stackoverflow.com/questions/75653875/jsoup-and-for-loop-nosuchelementexception/75658646#75658646 |%n");
         System.out.printf("|----------------------------------------------------|%n");
         System.out.printf("|                     Webscraping:                   |%n");
         System.out.printf("|----------------------------------------------------|%n");
